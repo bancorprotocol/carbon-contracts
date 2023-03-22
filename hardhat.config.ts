b2bd7d4bdf5779c93e3084336b7b5f0a308f6aba
Yudi Levi <yudi@bancor.network>
2023-03-22 08:57:11 +0000

    updated optimization runs to 18500

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -125,7 +125,7 @@ const config: HardhatUserConfig = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 200
+                        runs: 18500
                     },
                     metadata: {
                         bytecodeHash: 'none'
