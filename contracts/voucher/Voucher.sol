42ae5be37901965250e2a06f91c2fda5677e3174
Yudi Levi <yudi@bancor.network>
2023-03-22 08:57:57 +0000

    Voucher - removed unnecessary code

--- contracts/voucher/Voucher.sol
+++ contracts/voucher/Voucher.sol
@@ -95,7 +95,7 @@ contract Voucher is IVoucher, ERC721Enumerable, Utils, Ownable {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
         string memory baseURI = _baseURI();
-        if (_useGlobalURI == true) {
+        if (_useGlobalURI) {
             return baseURI;
         } else {
             return
