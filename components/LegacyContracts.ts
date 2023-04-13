31fea8913b7a68e68f0211d33aa32c86da27d608
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-04-13 14:09:52 +0300

    fix lint and format

--- components/LegacyContracts.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable camelcase */
-import { Signer } from 'ethers';
-
-const getContracts = (signer?: Signer) => ({
-    connect: (signer: Signer) => getContracts(signer)
-});
-
-export default getContracts();
