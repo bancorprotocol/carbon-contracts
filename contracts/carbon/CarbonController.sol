e901e44af249ab28e8c24de6d3338d5b5273edd7
barak manos <>
2023-04-17 19:23:39 +0300

    Check ETH value before checking if any of the participating tokens is ETH

--- contracts/carbon/CarbonController.sol
+++ contracts/carbon/CarbonController.sol
@@ -207,7 +207,7 @@ contract CarbonController is
         }
 
         // don't allow unnecessary eth
-        if (!stratPair.tokens[0].isNative() && !stratPair.tokens[1].isNative() && msg.value > 0) {
+        if (msg.value > 0 && !stratPair.tokens[0].isNative() && !stratPair.tokens[1].isNative()) {
             revert UnnecessaryNativeTokenReceived();
         }
 
@@ -438,7 +438,7 @@ contract CarbonController is
         _validateInputTokens(sourceToken, targetToken);
 
         // there shouldn't be any native token sent unless the source token is the native token
-        if (!sourceToken.isNative() && value > 0) {
+        if (value > 0 && !sourceToken.isNative()) {
             revert UnnecessaryNativeTokenReceived();
         }
     }
