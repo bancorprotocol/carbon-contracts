5a90809d32ca155d6db30ddf830a979b252a5faf
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-04-25 12:58:54 +0300

    setup delayed deployments

---
+++ deploy/tests/000100-revoke-roles.ts
@@ -0,0 +1,38 @@
+import { AccessControlEnumerableUpgradeable } from '../../components/Contracts';
+import { describeDeployment } from '../../utils/helpers/Deploy';
+import { DeployedContracts, isLive } from '../../utils/Deploy';
+import { expect } from 'chai';
+import { getNamedAccounts } from 'hardhat';
+import { Roles } from '../../utils/Roles';
+
+describeDeployment(__filename, () => {
+    let deployer: string;
+    let daoMultisig: string;
+
+    beforeEach(async () => {
+        ({ deployer, daoMultisig } = await getNamedAccounts());
+    });
+
+    it('should revoke deployer roles', async () => {
+        // get contracts
+        const carbon = (await DeployedContracts.CarbonController.deployed()) as AccessControlEnumerableUpgradeable;
+        const voucher = (await DeployedContracts.Voucher.deployed()) as AccessControlEnumerableUpgradeable;
+        const feeBurner = (await DeployedContracts.FeeBurner.deployed()) as AccessControlEnumerableUpgradeable;
+
+        // expect dao multisig to have the admin role for all contracts
+        expect(await carbon.hasRole(Roles.Upgradeable.ROLE_ADMIN, daoMultisig)).to.be.true;
+        expect(await voucher.hasRole(Roles.Upgradeable.ROLE_ADMIN, daoMultisig)).to.be.true;
+        expect(await feeBurner.hasRole(Roles.Upgradeable.ROLE_ADMIN, daoMultisig)).to.be.true;
+        
+        // expect deployer not to have the admin role for all contracts
+        expect(await carbon.hasRole(Roles.Upgradeable.ROLE_ADMIN, deployer)).to.be.false;
+        expect(await voucher.hasRole(Roles.Upgradeable.ROLE_ADMIN, deployer)).to.be.false;
+        expect(await feeBurner.hasRole(Roles.Upgradeable.ROLE_ADMIN, deployer)).to.be.false;
+
+        // expect deployer not to have the emergency stopper role
+        expect(await carbon.hasRole(Roles.CarbonController.ROLE_EMERGENCY_STOPPER, deployer)).to.be.false;
+    });
+},
+    
+{ skip: isLive }
+);
