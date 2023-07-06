03a6c9db82865b62e1ffbaed283b341b15215d82
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-07-06 14:30:18 +0300

    carbon vortex - pragma fix

--- contracts/vortex/interfaces/ICarbonVortex.sol
+++ contracts/vortex/interfaces/ICarbonVortex.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import { IUpgradeable } from "../../utility/interfaces/IUpgradeable.sol";
 import { Token } from "../../token/Token.sol";
