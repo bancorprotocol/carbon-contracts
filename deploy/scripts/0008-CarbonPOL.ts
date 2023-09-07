3547b5a3d4853d7b6543e8b725c5be1cef6dc034
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-09-07 16:33:23 +0300

    carbon pol - add deployment script

---
+++ deploy/scripts/0008-CarbonPOL.ts
@@ -0,0 +1,17 @@
+import { deployProxy, InstanceName, setDeploymentMetadata } from '../../utils/Deploy';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+const func: DeployFunction = async ({ getNamedAccounts }: HardhatRuntimeEnvironment) => {
+    const { deployer } = await getNamedAccounts();
+
+    await deployProxy({
+        name: InstanceName.CarbonPOL,
+        from: deployer,
+        args: []
+    });
+
+    return true;
+};
+
+export default setDeploymentMetadata(__filename, func);
