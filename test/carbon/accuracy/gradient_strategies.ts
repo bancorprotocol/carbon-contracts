edf08833673c0a8a228590a43f6de4cb5be97a39
barak manos <>
2024-08-05 17:30:04 +0300

    Remove `.only`

--- test/carbon/accuracy/gradient_strategies.ts
+++ test/carbon/accuracy/gradient_strategies.ts
@@ -68,7 +68,7 @@ function test(
     });
 }
 
-describe.only('Gradient strategies accuracy stress test', () => {
+describe('Gradient strategies accuracy stress test', () => {
     before(async () => {
         contract = await Contracts.TestTrade.deploy();
     });
