4bdf0f5cdbf5491401e79869e429a13beca7dbc3
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2024-08-27 13:10:24 +0300

    add carbon vortex upgrade health test

--- deploy/tests/network/0003-carbon-controller.ts
+++ deploy/tests/network/0003-carbon-controller.ts
@@ -30,7 +30,7 @@ describeDeployment(__filename, () => {
 
     it('cannot call postUpgrade on carbon controller', async () => {
         // hardcoding gas limit to avoid gas estimation attempts (which get rejected instead of reverted)
-        const tx = await carbonController.postUpgrade(false, '0x', { gasLimit: 6000000 });
+        const tx = await carbonController.postUpgrade(true, '0x', { gasLimit: 6000000 });
         await expect(tx.wait()).to.be.reverted;
     });
 });
